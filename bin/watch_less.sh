88b47d7731b3c3797347028df5b06fb69d7fbd87
Salimane Adjao Moustapha <me@salimane.com>
2012-03-25 07:12:04 +0800

    pipe the .less file to sed when compiling it

--- bin/watch_less.sh
+++ bin/watch_less.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Requires watchr: https://github.com/mynyml/watchr
-watchr -e 'watch(".*\.less$") { system("sless=`find -name style.less` && scss=`sed -e\"s/style.less/style.css/g\" \"$sless\"` && rm -rf \"$scss\" && lessc --compress \"$sless\" > \"$scss\" && notify-send \"Watchr Less\" \" Just compiled style.less > style.css\" -t 1 -i notification-message-im ") }'
+watchr -e 'watch(".*\.less$") { system("sless=`find -name style.less` && scss=`echo \"$sless\" | sed -e\"s/style.less/style.css/g\"` && rm -rf \"$scss\" && lessc --compress \"$sless\" > \"$scss\" && notify-send \"Watchr Less\" \" Just compiled style.less > style.css\" -t 1 -i notification-message-im ") }'

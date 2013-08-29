61dbbd1a8230a05deb98df61130af5dd565c62b6
Salimane Adjao Moustapha <me@salimane.com>
2013-08-29 21:12:20 +0200

    fix zsh crying about goenv

--- zsh/.zsh/etc/golang.zsh
+++ zsh/.zsh/etc/golang.zsh
@@ -5,5 +5,11 @@
 #export GOROOT="/usr/lib/go"
 #export GOARCH="amd64"
 #export GOOS="linux"
-export GOPATH="$HOME/src/mygo"
-export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+#export GOPATH="$HOME/src/mygo"
+#export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+if test -f /opt/boxen/goenv/bin/goenv
+then
+    export PATH="/opt/boxen/goenv/bin:$PATH"
+	eval "$(goenv init -)"
+fi
+

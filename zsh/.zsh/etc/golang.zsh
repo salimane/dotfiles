9560a437a1779d817696fd543e662a4e0d652878
Salimane Adjao Moustapha <me@salimane.com>
2014-01-16 14:36:42 +0100

    update go paths

--- zsh/.zsh/etc/golang.zsh
+++ zsh/.zsh/etc/golang.zsh
@@ -3,13 +3,10 @@
 ##############
 
 #export GOROOT="/usr/lib/go"
-#export GOARCH="amd64"
-#export GOOS="linux"
-#export GOPATH="$HOME/src/mygo"
-#export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+export GOPATH="$HOME/src/go"
+export PATH=$PATH:$GOPATH/bin
 if test -f /opt/boxen/goenv/bin/goenv
 then
     export PATH="/opt/boxen/goenv/bin:$PATH"
 	eval "$(goenv init -)"
 fi
-

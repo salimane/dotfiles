d1ac9fab01fe5d788fbe89beffe708ae98f9d583
Salimane Adjao Moustapha <me@salimane.com>
2014-12-05 11:48:18 +0100

    alias vagrant to the right Applications bin

--- zsh/.zsh/etc/alias.zsh
+++ zsh/.zsh/etc/alias.zsh
@@ -2,54 +2,54 @@
 # ALIASES #
 ###########
 
-alias cp="cp -i"
-alias rm="rm -i"
-alias mv="mv -i"
-alias ll='ls -l'
-alias la='ls -a'
-alias du="du -cksh"
-alias df="df -h"
-alias dir='dir --color=auto'
-alias vdir='vdir --color=auto'
-alias mkdir='mkdir -p'
-alias dus='du -ms * | sort -n'
-alias 'cd..=cd ..'
-alias tree="tree -A -I 'CVS|*~'"
-alias acs="sudo apt-cache search"
-alias acsh="sudo apt-cache show"
-alias agd="sudo apt-get dist-upgrade -m -y --force-yes "
-alias agi="sudo apt-get install -m -y --force-yes "
-alias agu="sudo apt-get update"
-alias agud="sudo apt-get update && sudo apt-get dist-upgrade -m -y --force-yes "
-alias addkey="sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys"
-alias info='info --vi-keys'
-alias less='less --tab=4 --no-init --LONG-PROMPT --ignore-case --squeeze-blank-line -R'
-alias get="curl -O"
-alias wgethtml='wget -E -H -k -K -p -nd -o logwget.txt'
-alias sed="sed -E"
-alias noascii="sed 's/.\\[[0-9][0-9]?m//g'"
-alias egrep='egrep --color=auto'
-alias grep=egrep
-alias fgrep='fgrep --color=auto'
-alias grpe=grep
-alias pgrep="\grep -P" # PCRE-compatible
-alias nano="nano -c"
-alias eject="drutil tray eject"
-alias quote="sed -Ee $'s/([ \'\"])/\\\\\\\\\\\1/g'"
-alias phpcs-fixer="phpcs-fixer --level=all --fixers=indentation,linefeed,trailing_spaces,php_closing_tag,short_tag,return,visibility,braces,phpdoc_params,eof_ending,extra_empty_lines,include,controls_spaces,elseif"
-alias phpunit="phpunit --coverage-text"
-alias update-submodules="git submodule sync; git submodule update --init"
-# force 256 color support in tmux
-alias tmux="tmux -2"
-# Make sure aliases are expanded when using sudo.
-alias sudo='sudo '
 #alias make="CFLAGS='-Wall' make"
-setopt complete_aliases
-export GREP_OPTIONS="--color=auto" # removes color when piping
-export GREP_COLOR='1;31' # highlight matches in red
-type colordiff >/dev/null 2>&1 && alias diff="colordiff -u" || alias diff="diff -u"
-type colormake >/dev/null 2>&1 && alias make=colormake
+alias acs='sudo apt-cache search'
+alias acsh='sudo apt-cache show'
+alias addkey='sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys'
+alias agd='sudo apt-get dist-upgrade -m -y --force-yes '
+alias agi='sudo apt-get install -m -y --force-yes '
+alias agu='sudo apt-get update'
+alias agud='sudo apt-get update && sudo apt-get dist-upgrade -m -y --force-yes '
+alias cd..='cd ..'
+alias cp='cp -i'
+alias df='df -h'
+alias dir='dir --color=auto'
+alias du='du -cksh'
+alias dus='du -ms * | sort -n'
+alias egrep='egrep --color=auto'
+alias eject='drutil tray eject'
+alias fgrep='fgrep --color=auto'
+alias get='curl -O'
+alias grep=egrep
+alias grpe=grep
+alias info='info --vi-keys'
+alias la='ls -a'
+alias less='less --tab=4 --no-init --LONG-PROMPT --ignore-case --squeeze-blank-line -R'
+alias ll='ls -l'
+alias mkdir='mkdir -p'
+alias mv='mv -i'
+alias nano='nano -c'
+alias noascii="sed 's/.\\[[0-9][0-9]?m//g'"
+alias pgrep="\grep -P" # PCRE-compatible
+alias phpcs-fixer='phpcs-fixer --level=all --fixers=indentation,linefeed,trailing_spaces,php_closing_tag,short_tag,return,visibility,braces,phpdoc_params,eof_ending,extra_empty_lines,include,controls_spaces,elseif'
+alias phpunit='phpunit --coverage-text'
+alias quote="sed -Ee $'s/([ \'\"])/\\\\\\\\\\\1/g'"
+alias rm='rm -i'
+alias sed='sed -E'
+alias sudo='sudo ' # Make sure aliases are expanded when using sudo.
+alias tmux='tmux -2' # force 256 color support in tmux
+alias tree="tree -A -I 'CVS|*~'"
+alias update-submodules='git submodule sync; git submodule update --init'
+alias vagrant='/Applications/Vagrant/bin/vagrant'
+alias vdir='vdir --color=auto'
+alias wgethtml='wget -E -H -k -K -p -nd -o logwget.txt'
 
+
+setopt complete_aliases
+export GREP_OPTIONS='--color=auto' # removes color when piping
+export GREP_COLOR='1;31' # highlight matches in red
+type colordiff >/dev/null 2>&1 && alias diff='colordiff -u' || alias diff='diff -u'
+type colormake >/dev/null 2>&1 && alias make=colormake
 
 PAGER=`which-command less`
 LESS=`which-command less`
@@ -72,7 +72,7 @@ if [[ -f ~/.shell_aliases ]]; then
 fi
 
 # Enable color support of ls
-if [[ "$TERM" != "dumb" ]]; then
+if [[ "$TERM" != 'dumb' ]]; then
     if [[ -x `which dircolors` ]]; then
         eval `dircolors -b`
         alias 'ls=ls --color=auto'

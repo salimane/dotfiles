a37104f3c43d639de140446ac679d95747210d6a
Salimane Adjao Moustapha <me@salimane.com>
2014-12-09 01:15:04 +0100

    disable goenv init

--- zsh/.zsh/etc/golang.zsh
+++ zsh/.zsh/etc/golang.zsh
@@ -2,11 +2,11 @@
 # GOLANG #
 ##############
 
-#export GOROOT="/usr/lib/go"
+# export GOROOT="/usr/lib/go"
 export GOPATH="$HOME/src/go"
 export PATH=$PATH:$GOPATH/bin
-if test -f /opt/boxen/goenv/bin/goenv
-then
-    export PATH="/opt/boxen/goenv/bin:$PATH"
-	eval "$(goenv init -)"
-fi
+# if test -f /opt/boxen/goenv/bin/goenv
+# then
+#     export PATH="/opt/boxen/goenv/bin:$PATH"
+# 	eval "$(goenv init -)"
+# fi

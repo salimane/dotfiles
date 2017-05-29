d3bb3059e621a7d6b8fe89b038eff0f21469f467
Salimane Adjao Moustapha <me@salimane.com>
2017-05-29 12:43:54 +0200

    Update rbenv bundle config
    Signed-off-by: Salimane Adjao Moustapha <me@salimane.com>

--- zsh/.zsh/etc/env.zsh
+++ zsh/.zsh/etc/env.zsh
@@ -1,2 +1,3 @@
-PATH=$PATH:~/bin
-PATH="$(python -m site --user-base)/bin:${PATH}"
+export PATH=$PATH:~/bin
+export PATH="$(python -m site --user-base)/bin:${PATH}"
+export PATH="/usr/local/sbin:$PATH"

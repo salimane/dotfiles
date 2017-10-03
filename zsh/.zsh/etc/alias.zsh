ea180d80521aac46ccaf903fac37617328f6738a
Salimane Adjao Moustapha <me@salimane.com>
2017-10-03 12:58:32 +0200

    Add ip related aliases
    Signed-off-by: Salimane Adjao Moustapha <me@salimane.com>

--- zsh/.zsh/etc/alias.zsh
+++ zsh/.zsh/etc/alias.zsh
@@ -37,6 +37,14 @@ alias update-submodules='git submodule sync; git submodule update --init'
 alias vdir='vdir --color=auto'
 alias wgethtml='wget -E -H -k -K -p -nd -o logwget.txt'
 
+# Get External IP / local IPs
+alias ip="curl ipinfo.io/ip"
+alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+alias speedtest="wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
+
+# Quickly serve the current directory as HTTP
+alias serve='ruby -run -e httpd . -p 8000'  # Or python -m SimpleHTTPServer :)
+
 
 setopt complete_aliases
 export GREP_OPTIONS='--color=auto' # removes color when piping

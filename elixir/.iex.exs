b5e274d4f7a66c0183b519a35b5e598f014690cd
Salimane Adjao Moustapha <me@salimane.com>
2017-11-02 21:58:19 +0100

    Update elixir iex prompt
    Signed-off-by: Salimane Adjao Moustapha <me@salimane.com>

--- elixir/.iex.exs
+++ elixir/.iex.exs
@@ -1,22 +1,5 @@
-timestamp = fn -> 
-  {_date, {hour, minute, _second}} = :calendar.local_time
-  [hour, minute]
-  |> Enum.map(&(String.pad_leading(Integer.to_string(&1), 2, "0")))
-  |> Enum.join(":")
-end
 
 Application.put_env(:elixir, :ansi_enabled, true)
-
-IEx.configure(default_prompt: "\r" <>
-  IO.ANSI.blue    <> "λ" <>
-  IO.ANSI.cyan    <> "(%counter)" <>
-  IO.ANSI.magenta <> ":" <> IO.ANSI.reset)
-
-IEx.configure(alive_prompt: "\r" <>
-  IO.ANSI.green   <> "%node:" <>
-  IO.ANSI.blue    <> "λ" <>
-  IO.ANSI.cyan    <> "(%counter)" <>
-  IO.ANSI.magenta <> ":" <> IO.ANSI.reset)
 
 IEx.configure(
   colors: [
@@ -34,18 +17,19 @@ IEx.configure(
     doc_headings: [:cyan, :underline],
     doc_title: [:cyan, :bright, :underline],
     eval_result: [:green, :bright] ,
-    eval_error: [[:red,:bright,"Bug Bug ..!!"]],
+    eval_error: [[:red,:bright]],
     eval_info: [:yellow, :bright ],
   ],
   default_prompt: 
-    "#{IO.ANSI.green}%prefix#{IO.ANSI.reset} " <>
-    "[#{IO.ANSI.magenta}#{timestamp.()}#{IO.ANSI.reset} " <>
-    ":: #{IO.ANSI.cyan}%counter#{IO.ANSI.reset}] >",
+    "\r" <>
+    IO.ANSI.magenta <> "λ☿" <>
+    IO.ANSI.magenta <> "⚡ " <> IO.ANSI.reset,
   alive_prompt: 
-    "#{IO.ANSI.green}%prefix#{IO.ANSI.reset} " <>
-    "(#{IO.ANSI.yellow}%node#{IO.ANSI.reset}) " <>
-    "[#{IO.ANSI.magenta}#{timestamp.()}#{IO.ANSI.reset} " <>
-    ":: #{IO.ANSI.cyan}%counter#{IO.ANSI.reset}] >",
+    "\r" <>
+    IO.ANSI.green <> "%node" <>
+    "\n" <>
+    IO.ANSI.magenta <> "λ☿" <>
+    IO.ANSI.magenta <> "⚡ " <> IO.ANSI.reset,
   history_size: 5000,
   inspect: [
     pretty: true, 

872973fcebc70ddbdbb2a6dfe716c1c0b44e4277
Salimane Adjao Moustapha <me@salimane.com>
2018-06-05 11:27:17 +0200

    Use asdf for java
    Signed-off-by: Salimane Adjao Moustapha <me@salimane.com>

--- zsh/.zsh/etc/java.zsh
+++ zsh/.zsh/etc/java.zsh
@@ -2,5 +2,16 @@
 # JAVA #
 ##############
 
-export JAVA_HOME="$(/usr/libexec/java_home)"
-export IDEA_JDK="1.8*"
+# export JAVA_HOME="$(/usr/libexec/java_home)"
+# export IDEA_JDK="1.8*"
+
+asdf_update_java_home() {
+  local current
+  if current=$(asdf current java); then
+    local version=$(echo $current | cut -d ' ' -f 1)
+    export JAVA_HOME=$(asdf where java $version)
+  else
+    echo "No java version set. Type `asdf list-all java` for all versions."
+  fi
+}
+asdf_update_java_home
